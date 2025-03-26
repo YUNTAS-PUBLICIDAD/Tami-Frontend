c035bd030af8014d64d9e876ce3c85162e80c203
Diego Quipuzco <demential2003@gmail.com>
2025-03-26 13:01:06 -0500

    se utilizo la variable de entorno

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -1,45 +1,50 @@
 import { useEffect, useState } from "react";
 import CardBlog from "./CardBlog";
 import type Blog from "src/models/Blog";
+import { getApiUrl, config } from "config";
 
 const BlogList = () => {
-    const [blogs, setBlogs] = useState<Blog[]>([]);
-    const [error, setError] = useState<string | null>(null);
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const apiUrl = "https://apitami.tami-peru.com/api/v1/blogs";
+  useEffect(() => {
+    const apiUrl = getApiUrl(config.endpoints.blogs.list);
 
-        const fetchBlogs = async () => {
-            try {
-                const response = await fetch(apiUrl);
-                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
 
-                const data = await response.json();
+        const data = await response.json();
 
-                if (data.success) {
-                    setBlogs(data.data);
-                } else {
-                    setError("No se pudieron obtener los blogs");
-                }
-            } catch (err) {
-                setError("Error al obtener los blogs: " + (err instanceof Error ? err.message : "Error desconocido"));
-            }
-        };
+        if (data.success) {
+          setBlogs(data.data);
+        } else {
+          setError("No se pudieron obtener los blogs");
+        }
+      } catch (err) {
+        setError(
+          "Error al obtener los blogs: " +
+            (err instanceof Error ? err.message : "Error desconocido")
+        );
+      }
+    };
 
-        fetchBlogs();
-    }, []);
+    fetchBlogs();
+  }, []);
 
-    return (
-        <>
-            {error ? (
-                <p className="text-center text-red-600">{error}</p>
-            ) : blogs.length > 0 ? (
-                blogs.map((blog) => <CardBlog key={blog.id} blog={blog} />)
-            ) : (
-                <p className="text-center text-teal-800">No hay blogs disponibles.</p>
-            )}
-        </>
-    );
+  return (
+    <>
+      {error ? (
+        <p className="text-center text-red-600">{error}</p>
+      ) : blogs.length > 0 ? (
+        blogs.map((blog) => <CardBlog key={blog.id} blog={blog} />)
+      ) : (
+        <p className="text-center text-teal-800">No hay blogs disponibles.</p>
+      )}
+    </>
+  );
 };
 
 export default BlogList;
