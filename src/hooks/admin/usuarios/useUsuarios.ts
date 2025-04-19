35da829fb35a8193718286f4b89b9cef638c0bf0
Anstro <anstrosolano@gmail.com>
2025-04-19 10:44:11 -0500

    Agregar enums y hooks para la gestión de usuarios

---
+++ src/hooks/admin/usuarios/useUsuarios.ts
@@ -0,0 +1,87 @@
+/**
+ * @file useusuarios.ts
+ * @description Este archivo contiene el hook para obtener la lista de usuarios.
+ * @returns {Object} Un objeto que contiene la lista de usuarios, un estado de carga y un mensaje de error.
+ */
+
+import { useState, useEffect } from "react";
+import { getApiUrl, config } from "config";
+import type Usuario from "../../../models/Users";
+
+const useUsuarios = (trigger: boolean, page: number = 1) => {
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]); // Cambia el tipo a usuario[] para reflejar la estructura de datos
+  const [totalPages, setTotalPages] = useState<number>(1); // Número total de páginas
+  const [loading, setLoading] = useState<boolean>(true); // Estado de carga
+  const [error, setError] = useState<string | null>(null); // Mensaje de error
+
+  useEffect(() => {
+    /**
+     * Función para obtener la lista de usuarios desde la API.
+     * Maneja el estado de carga y errores.
+     */
+    const fetchUsuarios = async () => {
+      setLoading(true);
+      setError(null);
+
+      /**
+       * Obtiene el token de autenticación del localStorage y realiza la solicitud a la API.
+       * Si no se encuentra el token, lanza un error.
+       */
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) throw new Error("No se encontró el token de autenticación");
+
+        /**
+         * Realiza la solicitud a la API para obtener la lista de usuarios.
+         */
+        const url = `${getApiUrl(config.endpoints.users.list)}?page=${page}`;
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok)
+          throw new Error(`Error al obtener usuarios: ${response.statusText}`);
+
+        /**
+         * Convierte la respuesta a JSON y maneja los datos.
+         */
+        const data = await response.json();
+
+        /**
+         * Extrae la lista de usuarios y el número total de páginas de la respuesta.
+         * Si no hay datos, establece un array vacío y una página total de 1.
+         */
+        const UsuariosArray = data.data?.data || [];
+        const totalPages = Math.ceil(data.data?.total / 10) || 1;
+        setUsuarios(UsuariosArray);
+        setTotalPages(totalPages);
+
+        /**
+         * Manejo de errores en la solicitud y respuesta.
+         */
+      } catch (err) {
+        console.error("🚨 Error en fetchusuarios:", err);
+        setError(
+          err instanceof Error ? err.message : "Ocurrió un error desconocido"
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsuarios();
+  }, [trigger, page]); //
+
+  return {
+    usuarios, // Lista de usuarios
+    totalPages, // Número total de páginas
+    loading, // Estado de carga
+    error, // Mensaje de error
+  };
+};
+
+export default useUsuarios;
---
+++ src/hooks/admin/usuarios/useUsuariosActions.ts
@@ -0,0 +1,101 @@
+/**
+ * @file useUsuariosActions.ts
+ * @description Este archivo contiene los hooks para las acciones de los Usuarios.
+ */
+
+import { getApiUrl, config } from "config"; // importa la configuración de la API
+import type Usuario from "../../../models/Users"; // importa el modelo de Usuario
+
+const useUsuarioAcciones = () => {
+  
+  /**
+   * Obtiene el token de autenticación del localStorage y realiza la solicitud a la API.
+   * Si no se encuentra el token, lanza un error.
+   */
+
+  const getValidToken = () => {
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("No se encontró el token");
+    return token;
+  };
+
+  /**
+   * Funcion para añadir un Usuario
+   */
+
+  const addUsuario = async (
+    UsuarioData: Partial<Usuario>
+  ): Promise<Usuario> => {
+    const token = getValidToken(); 
+    const url = getApiUrl(config.endpoints.users.create); 
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(UsuarioData),
+    });
+
+    if (!response.ok) throw new Error("Error al agregar Usuario");
+
+    const result: { data: Usuario } = await response.json();
+    return result.data;
+  };
+
+  /**
+   * Función para actualizar un Usuario, usando los tipos
+   */
+
+  const updateUsuario = async (
+    id: number,
+    updatedData: Partial<Usuario>
+  ): Promise<Usuario> => {
+    const token = getValidToken();
+    const url = getApiUrl(config.endpoints.users.update(id));
+
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(updatedData),
+    });
+
+    if (!response.ok) throw new Error("Error al actualizar Usuario");
+
+    const result: { data: Usuario } = await response.json();
+    return result.data;
+  };
+
+  /**
+   * Función para eliminar un Usuario, usando los tipos
+   */
+  
+  const deleteUsuario = async (id: number): Promise<{ message: string }> => {
+    const token = getValidToken();
+    const url = getApiUrl(config.endpoints.users.delete(id));
+
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) throw new Error("Error al eliminar Usuario");
+
+    const result: { message: string } = await response.json();
+    return result;
+  };
+
+  return {
+    addUsuario,
+    updateUsuario,
+    deleteUsuario,
+  };
+};
+
+export default useUsuarioAcciones;
