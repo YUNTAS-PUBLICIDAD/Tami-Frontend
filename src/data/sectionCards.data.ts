9dbff4e6cfda3f62ad4d691bf03f12928a800c01
fati868 <dqh927401@gmail.com>
2025-05-02 15:25:20 -0500

    cambios en about, Card y SectionCard

--- src/data/sectionCards.data.ts
+++ src/data/sectionCards.data.ts
@@ -1,24 +1,25 @@
 import bg_card1 from "@images/index/cards/card_trabajo.webp";
 import bg_card2 from "@images/index/cards/card_decoracion_y_hogar.webp";
 import bg_card3 from "@images/index/cards/card_negocio.webp";
+
 const sectionCards = [
   {
-    id: "Trabajo",
-    title: "TRABAJO",
+    id: "Negocio",
+    title: "NEGOCIO",
     paragraph:
       "Equipos diseñados para\noptimizar y agilizar tus\nprocesos laborales.",
     image: bg_card1,
   },
   {
     id: "Decoración",
-    title: "DECORACIÓN Y HOGAR",
+    title: "DECORACIÓN",
     paragraph:
       "Productos que combinan\nfuncionalidad y estilo para\ntu espacio.",
     image: bg_card2,
   },
   {
-    id: "Negocio",
-    title: "NEGOCIO",
+    id: "Maquinaria",
+    title: "MAQUINARIA",
     paragraph:
       "Herramientas innovadoras\nque impulsan el crecimiento\nde tu negocio.",
     image: bg_card3,
