1a9950114d6ecbeef4adccf40705eabeea8a8420
Alex <alexcantsed@hotmail.com>
2025-06-11 17:49:49 -0500

    hotfix: Para el popup y function para productos relacionados

--- src/components/ui/ScrollModal.tsx
+++ src/components/ui/ScrollModal.tsx
@@ -38,7 +38,7 @@ const ScrollModal = () => {
         setTimeout(() => {
             setShowModal(false);
             setIsClosing(false);
-        }, 8000);
+        }, 300);
     };
 
     if (!showModal) return null;
