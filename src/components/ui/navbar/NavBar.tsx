099a57404a04a495f81e3b97e6e565f82ff445e8
AlexlmL <alexcantsed@hotmail.com>
2025-07-09 17:23:13 -0500

    Fix: Navbar actualizada
    TODO: Barra de busqueda e ícono

--- src/components/ui/navbar/NavBar.tsx
+++ src/components/ui/navbar/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import logoMovil from "@images/logos/logo_movil.webp";
 import logoTami from "@images/logos/logo_web.webp";
+import whatsappIcon from "../../../assets/icons/smi_whatsapp.svg";
 import NavLink from "./NavLink";
 import SideMenu from "../sideMenu/SideMenu";
 import navLinks from "@data/navlinks.data";
@@ -70,7 +71,7 @@ function NavBar() {
                 {item.texto}
               </NavLink>
             </li>
-          ))}
+          ))}{/*
           <li className="relative group text-white hover:text-teal-300 transition-colors duration-300 text-lg sm:text-xl font-bold cursor-pointer">
             Más
             <span className="absolute bottom-0 left-1/2 w-0 h-0.5 bg-teal-300 transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
@@ -92,17 +93,24 @@ function NavBar() {
                 </a>
               </li>
             </ul>
-          </li>
+          </li>*/}
         </ul>
       </nav>
-      
-      <div className="hidden lg:block col-span-2 h-full content-center text-end w-full">
+
+      <div className="hidden lg:flex col-span-2 h-full content-center text-end w-full items-center justify-end gap-4">
+        <a href="https://api.whatsapp.com/send?phone=51978883199" target="_blank" rel="noopener noreferrer">
+          <img
+              src={whatsappIcon.src}
+              alt="WhatsApp"
+              className="w-7 h-7"
+          />
+        </a>
         <a
-          href="https://api.whatsapp.com/send?phone=51978883199"
-          target="_blank"
-          className="w-fit bg-white rounded-2xl border-2 border-slate-300 py-2 px-5 text-teal-700 text-base sm:text-lg hover:bg-linear-to-t hover:from-teal-600 hover:to-teal-800 hover:text-white transition-all ease-in-out duration-300 font-bold"
+            href="/auth/sign-in"
+            target="_blank"
+            className="w-fit bg-white rounded-2xl border-2 border-slate-300 py-2 px-5 text-teal-700 text-base sm:text-lg hover:bg-linear-to-t hover:from-teal-600 hover:to-teal-800 hover:text-white transition-all ease-in-out duration-300 font-bold"
         >
-          Contáctanos
+          LOGIN
         </a>
       </div>
     </header>
