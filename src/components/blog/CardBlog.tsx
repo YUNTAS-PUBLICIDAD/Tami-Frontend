9a0b52b50e93313880ddf03d878d960ee64846c0
ImanolZela <imanolza1304@gmail.com>
2025-09-27 12:42:33 -0500

    Refactorizacion y limpieza de codigo

--- src/components/blog/CardBlog.tsx
+++ src/components/blog/CardBlog.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
 import type Blog from "src/models/Blog";
 import { config } from "config";
@@ -6,9 +7,11 @@ interface CardBlogProps {
   blog: Blog;
 }
 
-const CardBlog: React.FC<CardBlogProps> = ({ blog }) => {
-  const apiUrl = config.apiUrl;
+const apiUrl = config.apiUrl;
 
+const CardBlog: React.FC<CardBlogProps> = React.memo(({ blog }) => {
+  const miniatura = blog.miniatura ? `${apiUrl}${blog.miniatura}` : "/images/default-blog.webp";
+  const altImg = blog.titulo || "Imagen del blog";
   return (
     <a
       href={`/blog/details?id=${blog.id}`}
@@ -17,21 +20,21 @@ const CardBlog: React.FC<CardBlogProps> = ({ blog }) => {
     >
       <figure className="lg:w-1/3 w-full h-48 flex items-center justify-center bg-white rounded-l-xl overflow-hidden">
         <img
-          src={`${apiUrl}${blog.miniatura}`}
-          alt={blog.titulo}
-          title={blog.titulo}
+          src={miniatura}
+          alt={altImg}
+          title={altImg}
           loading="lazy"
           className="w-full h-full object-cover transition-all duration-300"
         />
       </figure>
       <div className="lg:w-2/3 w-full p-6">
-        {blog.nombre_producto && (
+        {blog.nombre_producto ? (
           <div className="flex items-center mb-2">
             <span className="border border-white rounded px-2 py-1 mr-2 ml-3">
               {blog.nombre_producto}
             </span>
           </div>
-        )}
+        ) : null}
         <h2 className="text-2xl font-bold mb-2 ml-3">{blog.subtitulo1}</h2>
         <div className="flex flex-row items-center gap-2">
           <MdOutlineArrowForwardIos className="text-5xl" />
@@ -40,6 +43,6 @@ const CardBlog: React.FC<CardBlogProps> = ({ blog }) => {
       </div>
     </a>
   );
-};
+});
 
 export default CardBlog;
