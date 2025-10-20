feb8bee2aee43881a1d1c5012cf5d592d8a930f9
Junior <105558189+RoqueZ12@users.noreply.github.com>
2025-10-20 11:53:24 -0500

    Juniortami (#230)
    * actualizacion diseño blog
    
    * actualizacion diseño nosotros

--- src/components/blog/BlogPage.tsx
+++ src/components/blog/BlogPage.tsx
@@ -1,32 +1,34 @@
 import { useState } from "react";
 import BlogList from "./BlogList";
 import { FaSearch } from "react-icons/fa";
-// import {IoIosArrowDown} from "react-icons/io";
 
 const BlogPage = () => {
-    const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <section className="container mx-auto p-4 md:p-10 lg:px-24">
-            <h2 className="text-teal-800 text-3xl font-bold mb-4 mx-4">
-                Todos los artículos</h2>
-            <div className="flex flex-wrap items-center gap-4 mx-4 mb-6">
-                <div className="relative flex items-center flex-grow">
-                    <FaSearch className="absolute left-3 text-teal-800" />
-                    <input
-                        type="text"
-                        placeholder="Buscar..."
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        className="w-full border border-teal-800 rounded-3xl pl-10 pr-4 py-2 text-teal-800 focus:outline-none focus:border-2 focus:border-teal-800"
-                    />
-                </div>
-            </div>
-            <BlogList searchTerm={searchTerm} />
-        </section>
-    );
+  return (
+    <section className="container mx-auto p-4 md:p-10 lg:px-24">
+      <h2 className="text-teal-700 text-3xl md:text-4xl font-bold mb-6">
+        Todos los productos
+      </h2>
+
+      <div className="mb-8">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Buscar"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full border-2 border-teal-700 rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 
+              focus:outline-none focus:border-[#00786F]
+              hover:border-[#00786F] transition-all duration-300"
+          />
+          <FaSearch className="absolute right-4 top-3.5 text-teal-700" />
+        </div>
+      </div>
+
+      <BlogList searchTerm={searchTerm} />
+    </section>
+  );
 };
+
 export default BlogPage;
-
-
-
