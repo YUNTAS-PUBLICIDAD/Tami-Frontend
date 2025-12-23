6a49aa257ee39e4b12237f67beb7d0cbb2cd5b23
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-12-23 17:43:52 -0500

    Apartado Editar, tamaño de video

--- src/components/products/ProductPage.tsx
+++ src/components/products/ProductPage.tsx
@@ -131,7 +131,7 @@ const ProductPage: React.FC<Props> = ({ producto }) => {
                                     />
                                 </div>
                                 <div className="grid grid-cols-4 gap-4 w-full max-w-xl">
-                                    {producto.imagenes?.slice(0, 4).map((image) => (
+                                    {producto.imagenes?.slice(1, 5).map((image) => (
                                         <div
                                             key={image.url_imagen}
                                             className={`aspect-square  bg-white rounded-xl overflow-hidden shadow-xl cursor-pointer flex items-center justify-center p-2 border-2 ${image.url_imagen === productViewer ? 'border-blue-500' : 'border-gray-200'} transition-all duration-300`}
